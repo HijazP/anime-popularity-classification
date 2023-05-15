d0ce3c700ca11ae0eedbdc4d2f2a282c02e76191
Aysuka Ansari <aysukaansari@apps.ipb.ac.id>
2023-05-15 22:24:41 +0700

    membersihkan records

--- main.R
+++ main.R
@@ -5,3 +5,15 @@ data <- read.csv("./dataset/anime_cleaned.csv")
 # Preprocessing
 ## Feature selection
 df <- data.frame(data[,c(1:2,7:9,14:21,23,28:29)])
+
+## Menghapus record yang tidak perlu
+count_genre_hentai <- sum(df$genre == "Hentai")
+count_genre_hentai
+count_rating_nudity <- sum(df$rating == "R+ - Mild Nudity")
+count_rating_nudity
+count_rating_hentai <- sum(df$rating == "Rx - Hentai")
+count_rating_hentai
+total_deleted_records <- count_genre_hentai + count_rating_nudity + count_rating_hentai
+df <- df[df$genre != "Hentai", ]
+df <- df[df$rating != "R+ - Mild Nudity", ]
+df <- df[df$rating != "Rx - Hentai", ]
